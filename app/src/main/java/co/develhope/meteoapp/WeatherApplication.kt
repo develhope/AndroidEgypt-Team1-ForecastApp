8ed8ac25509243ee80bab2522914a20e7f0395bf
Zyad Mohamed <zyadmuhammed05@gmail.com>
2024-02-13 19:01:31 +0200

    UpdateTodayViewModel And it's dpedency Injection

--- app/src/main/java/co/develhope/meteoapp/WeatherApplication.kt
+++ app/src/main/java/co/develhope/meteoapp/WeatherApplication.kt
@@ -1,5 +1,8 @@
 package co.develhope.meteoapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
 
+@HiltAndroidApp
 class WeatherApplication : Application()
